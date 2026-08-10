8b33ab62242246b267dbfe49b1c286553dc4f015
zeraX23 <officialjiyora@gmail.com>
2026-08-10 12:18:50 +0700

    Update service worker cache version and logic
    Updated cache name to 'shopee-ads-v3' for forced updates on devices. Added cache deletion for old versions during activation.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "shopee-ads-v2";
+const CACHE_NAME = "shopee-ads-v3"; // Versi cache dinaikkan untuk memaksa update di HP
 const assets = [
   "./",
   "./index.html",
@@ -12,7 +12,19 @@ const assets = [
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", activateEvent => {
+  activateEvent.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))
+      );
     })
   );
 });
